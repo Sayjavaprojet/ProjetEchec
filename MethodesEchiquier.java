37da8c1863e135e5b787bc494bf39e51f52f9669
Sayjavaprojet <dbz93@live.fr>
2017-06-01 01:13:31 +0200

    Add files via upload

---
+++ MethodesEchiquier.java
@@ -0,0 +1,10 @@
+package Jeux;
+
+
+public interface MethodesEchiquier 
+{
+    public abstract void debuter();
+    public abstract Case getCase(int ligne, int colonne);
+    public abstract boolean cheminPossible (Deplacement deplacement);
+    public abstract boolean captureParUnPionPossible(Deplacement deplacement);
+}
