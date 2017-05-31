6ce062c334eddcc3450bab43d65205f28bbe9884
Sayjavaprojet <dbz93@live.fr>
2017-06-01 01:11:32 +0200

    Add files via upload

---
+++ Cavalier.java
@@ -0,0 +1,20 @@
+package Jeux;
+
+public class Cavalier extends Piece {
+
+	 public Cavalier(String Couleur) {
+	        super("Cavalier", Couleur);
+	    }
+	 
+	 
+	
+	 public boolean estValide(Deplacement deplacement) {
+	      
+	       return (Math.abs(deplacement.getDeplacementX() / deplacement.getDeplacementY())) == 2 |
+	                (Math.abs(deplacement.getDeplacementX() / deplacement.getDeplacementY())) == .5;
+	    }
+	}
+	
+	
+	
+}
---
+++ Reine.java
@@ -0,0 +1,14 @@
+package Jeux;
+
+public class Reine extends Piece{
+    
+    public Reine(String Couleur) {
+        super("Reine", Couleur);
+    }
+
+    public boolean estValide(Deplacement deplacement) {
+ 
+        return (Math.abs(deplacement.getDeplacementX()) - Math.abs(deplacement.getDeplacementY()) == 0 
+                | deplacement.getDeplacementX() * deplacement.getDeplacementY() == 0) && !deplacement.isNul();
+    }
+}
